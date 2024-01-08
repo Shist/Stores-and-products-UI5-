67d83e0f6ba3839498e1a52ba879e7805f86e280
Shist <shist.pupust@mail.ru>
2024-01-08 17:23:37 +0300

    HW 5, try 1 : Changed labels for pages (from pages to stores/products)

--- webapp/controller/StoreDetails.controller.js
+++ webapp/controller/StoreDetails.controller.js
@@ -7,6 +7,7 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
         .getRouter()
         .navTo("ProductDetails", { storeId: 1, productId: 1 });
     },
+
     onNavToStoresOverview: function () {
       this.getOwnerComponent().getRouter().navTo("StoresOverview");
     },
