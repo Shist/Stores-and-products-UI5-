95dddeccb4ba74341892c1b9b803c9aad088b896
Shist <shist.pupust@mail.ru>
2024-01-14 02:17:19 +0300

    HW 6, try 1 : Added loading of comments of current product

--- webapp/controller/ProductDetails.controller.js
+++ webapp/controller/ProductDetails.controller.js
@@ -1,48 +1,72 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+  ],
+  function (Controller, Filter, FilterOperator) {
+    "use strict";
 
-  return Controller.extend("pavel.zhukouski.controller.ProductDetails", {
-    onInit: function () {
-      const oRouter = this.getOwnerComponent().getRouter();
-      oRouter
-        .getRoute("ProductDetails")
-        .attachPatternMatched(this.onRouterPatternMatched, this);
-    },
+    return Controller.extend("pavel.zhukouski.controller.ProductDetails", {
+      onInit: function () {
+        const oRouter = this.getOwnerComponent().getRouter();
+        oRouter
+          .getRoute("ProductDetails")
+          .attachPatternMatched(this.onRouterPatternMatched, this);
+      },
 
-    onExit: function () {
-      const oRouter = this.getOwnerComponent().getRouter();
-      oRouter
-        .getRoute("ProductDetails")
-        .detachPatternMatched(this.onRouterPatternMatched, this);
-    },
+      onAfterRendering: function () {
+        const oCommentsBinding = this.byId("commentsList").getBinding("items");
 
-    onRouterPatternMatched: function (oEvent) {
-      const controllerContext = this;
-      const sProductId = oEvent.getParameter("arguments").productId;
-      const oODataModel = this.getView().getModel("odata");
+        setTimeout(() => {
+          const nProductId = this.getView()
+            .getBindingContext("odata")
+            .getObject("id");
 
-      oODataModel.metadataLoaded().then(function () {
-        const sKey = oODataModel.createKey("/Products", { id: sProductId });
+          oCommentsBinding.filter(
+            new Filter("ProductId", FilterOperator.EQ, nProductId)
+          );
+        }, 100); // TODO: Somehow refactor this cringe. We need to wait before productId will appear inside Binding
+      },
 
-        controllerContext.getView().bindObject({
-          path: sKey,
-          model: "odata",
+      onExit: function () {
+        const oRouter = this.getOwnerComponent().getRouter();
+        oRouter
+          .getRoute("ProductDetails")
+          .detachPatternMatched(this.onRouterPatternMatched, this);
+      },
+
+      onRouterPatternMatched: function (oEvent) {
+        const controllerContext = this;
+        const sProductId = oEvent.getParameter("arguments").productId;
+        const oODataModel = this.getView().getModel("odata");
+
+        oODataModel.metadataLoaded().then(function () {
+          const sKey = oODataModel.createKey("/Products", { id: sProductId });
+
+          controllerContext.getView().bindObject({
+            path: sKey,
+            model: "odata",
+          });
         });
-      });
-    },
+      },
 
-    onStoresListLinkClicked: function () {
-      this.getOwnerComponent().getRouter().navTo("StoresOverview");
-    },
+      onStoresListLinkClicked: function () {
+        const oAppViewModel = this.getView().getModel("appView");
+        oAppViewModel.setProperty("/currStatusFilter", "ALL");
 
-    onStoreDetailsLinkClicked: function () {
-      const nStoreId = this.getView()
-        .getBindingContext("odata")
-        .getObject("StoreId");
+        this.getOwnerComponent().getRouter().navTo("StoresOverview");
+      },
 
-      this.getOwnerComponent().getRouter().navTo("StoreDetails", {
-        storeId: nStoreId,
-      });
-    },
-  });
-});
+      onStoreDetailsLinkClicked: function () {
+        const nStoreId = this.getView()
+          .getBindingContext("odata")
+          .getObject("StoreId");
+
+        this.getOwnerComponent().getRouter().navTo("StoreDetails", {
+          storeId: nStoreId,
+        });
+      },
+    });
+  }
+);
