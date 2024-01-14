629da85f69641c61c7f46adc245673aa2ea7e13d
Shist <shist.pupust@mail.ru>
2024-01-14 07:48:38 +0300

    HW 6, try 1 : Added sort btns for products columns with appView obj

--- webapp/controller/StoreDetails.controller.js
+++ webapp/controller/StoreDetails.controller.js
@@ -102,6 +102,73 @@ sap.ui.define(
         return sStorePath;
       },
 
+      onSortBtnClicked(sBtnKey) {
+        const oAppViewModel = this.getView().getModel("appView");
+        const sCurrSortState = oAppViewModel.getProperty(
+          `/columnsSortStates/${sBtnKey}`
+        );
+
+        const newSortObj = {
+          name: "DEFAULT",
+          price: "DEFAULT",
+          specs: "DEFAULT",
+          supplierInfo: "DEFAULT",
+          country: "DEFAULT",
+          prodCompany: "DEFAULT",
+          rating: "DEFAULT",
+        };
+
+        if (sCurrSortState === "DEFAULT") {
+          newSortObj[sBtnKey] = "ASC";
+        } else if (sCurrSortState === "ASC") {
+          newSortObj[sBtnKey] = "DESC";
+        }
+
+        oAppViewModel.setProperty("/columnsSortStates", newSortObj);
+      },
+
+      onSortNameBtnClicked: function () {
+        this.onSortBtnClicked("name");
+      },
+
+      onSortPriceBtnClicked: function () {
+        this.onSortBtnClicked("price");
+      },
+
+      onSortSpecsBtnClicked: function () {
+        this.onSortBtnClicked("specs");
+      },
+
+      onSortSupplierBtnClicked: function () {
+        this.onSortBtnClicked("supplierInfo");
+      },
+
+      onSortCountryBtnClicked: function () {
+        this.onSortBtnClicked("country");
+      },
+
+      onSortProdCompanyBtnClicked: function () {
+        this.onSortBtnClicked("prodCompany");
+      },
+
+      onSortRatingBtnClicked: function () {
+        this.onSortBtnClicked("rating");
+      },
+
+      formatSortBtnIcon: function (sSortState) {
+        switch (sSortState) {
+          case "DEFAULT":
+            return "sap-icon://sort";
+          case "ASC":
+            return "sap-icon://sort-ascending";
+          case "DESC":
+            return "sap-icon://sort-descending";
+          default:
+            console.warn(`Got unknown type of sort state: ${sSortState}`);
+            return "sap-icon://sys-help";
+        }
+      },
+
       onStoresListLinkClicked: function () {
         const oAppViewModel = this.getView().getModel("appView");
         oAppViewModel.setProperty("/currStatusFilter", "ALL");
