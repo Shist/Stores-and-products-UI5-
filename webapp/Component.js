625f7fb2ae00d1852b13eebf812b8a92643b6266
Shist <shist.pupust@mail.ru>
2024-01-16 05:24:30 +0300

    HW 6, try 1 : Added search filter for stores list

--- webapp/Component.js
+++ webapp/Component.js
@@ -7,13 +7,9 @@ sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
     },
 
     init: function () {
-      // call the init function of the parent
       UIComponent.prototype.init.apply(this, arguments);
 
       this.getRouter().initialize();
-
-      window.oDataModel = this.getModel("odata"); // <-- TODO: Remove this after debugging
-      window.appViewModel = this.getModel("appView"); // <-- TODO: Remove this after debugging
     },
   });
 });
--- webapp/controller/StoresOverview.controller.js
+++ webapp/controller/StoresOverview.controller.js
@@ -1,16 +1,66 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+  ],
+  function (Controller, Filter, FilterOperator) {
+    "use strict";
 
-  return Controller.extend("pavel.zhukouski.controller.StoresOverview", {
-    onStoreClick: function (oEvent) {
-      const nStoreId = oEvent
-        .getSource()
-        .getBindingContext("odata")
-        .getObject("id");
+    return Controller.extend("pavel.zhukouski.controller.StoresOverview", {
+      onAfterRendering: function () {
+        const oStoresBinding = this.byId("storesList").getBinding("items");
+        window.storesBinding = oStoresBinding;
+      },
 
-      this.getOwnerComponent().getRouter().navTo("StoreDetails", {
-        storeId: nStoreId,
-      });
-    },
-  });
-});
+      onStoreClick: function (oEvent) {
+        const nStoreId = oEvent
+          .getSource()
+          .getBindingContext("odata")
+          .getObject("id");
+
+        this.getOwnerComponent().getRouter().navTo("StoreDetails", {
+          storeId: nStoreId,
+        });
+      },
+
+      onStoresSearchBtnClick: function (oEvent) {
+        const oStoresBinding = this.byId("storesList").getBinding("items");
+        const sQuery = oEvent.getParameter("query");
+        let targetFilter = [];
+
+        oStoresBinding.refresh();
+
+        if (sQuery && sQuery.length > 0) {
+          const aFilters = [];
+
+          const filterName = new Filter(
+            "Name",
+            FilterOperator.Contains,
+            sQuery
+          );
+          aFilters.push(filterName);
+
+          const filterAddress = new Filter(
+            "Address",
+            FilterOperator.Contains,
+            sQuery
+          );
+          aFilters.push(filterAddress);
+
+          const filterFloorArea = new Filter({
+            path: "FloorArea",
+            operator: FilterOperator.EQ,
+            value1: sQuery,
+            comparator: (a, b) => a - b,
+          });
+          aFilters.push(filterFloorArea);
+
+          targetFilter = new Filter({ filters: aFilters, and: false });
+        }
+
+        oStoresBinding.filter(targetFilter);
+      },
+    });
+  }
+);
